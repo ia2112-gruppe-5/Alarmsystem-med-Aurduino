bb8d4fa0936a9db59ba10720f6b8dfbc928e9600
Gronning <47217317+Gronning@users.noreply.github.com>
2019-03-30 12:42:06 +0100

    Delete AlarmData.sql

--- AlarmData.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[AlarmData]
-(
-	[Mailaddresse] NVARCHAR(100) NOT NULL, 
-    [AlarmId] TIMESTAMP NOT NULL,
-    CONSTRAINT [FK_AlarmData_Abonenter] FOREIGN KEY ([Mailaddresse]) REFERENCES [Abonenter]([Mailaddresse]), 
-    CONSTRAINT [FK_AlarmData_Alarmer] FOREIGN KEY ([AlarmId]) REFERENCES [Alarmer]([AlarmId]),
-	CONSTRAINT [PK_AlarmData] PRIMARY KEY ([Mailaddresse], [AlarmId])
-)
