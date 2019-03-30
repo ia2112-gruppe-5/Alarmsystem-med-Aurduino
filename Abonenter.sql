f6078057a9b6ba4829487ac91021e610f145c5c4
Gronning <47217317+Gronning@users.noreply.github.com>
2019-03-30 12:41:57 +0100

    Delete Abonenter.sql

--- Abonenter.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Abonenter]
-(
-	[Mailaddresse] NVARCHAR(100) NOT NULL PRIMARY KEY, 
-    [Fornavn] NVARCHAR(50) NULL, 
-    [Etternavn] NVARCHAR(50) NULL, 
-    [Telefonnummer] INT NULL, 
-	[Postnummer] CHAR(4) NOT NULL,
-    CONSTRAINT [FK_Abonenter_Postaddresse] FOREIGN KEY (Postnummer) REFERENCES [Postaddresse]([Postnummer])
-)
