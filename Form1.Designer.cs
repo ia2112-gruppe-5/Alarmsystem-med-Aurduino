d3454cc93fa1ef7cf6eb4c65d17e7d75780ae6a4
TheDobler <46963445+TheDobler@users.noreply.github.com>
2019-04-11 13:13:11 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Cs_Program_arduinoV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(24, 23);
+            this.txtBox.Margin = new System.Windows.Forms.Padding(6);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(872, 237);
+            this.txtBox.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cboBox
+            // 
+            this.cboBox.FormattingEnabled = true;
+            this.cboBox.Items.AddRange(new object[] {
+            "1",
+            "5",
+            "10",
+            "15",
+            "30"});
+            this.cboBox.Location = new System.Drawing.Point(24, 319);
+            this.cboBox.Name = "cboBox";
+            this.cboBox.Size = new System.Drawing.Size(121, 33);
+            this.cboBox.TabIndex = 3;
+            this.cboBox.SelectedIndexChanged += new System.EventHandler(this.cboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.cboBox);
+            this.Controls.Add(this.txtBox);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox cboBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Cs_Program_arduinoV1
+{
+    public partial class Form1 : Form
+    {
+        static ArduinoComClass ArduinoCom;
+        Thread oThreadone;
+        public static SerialPort sendToArduino = new SerialPort();
+        SensorData liveSensorData;
+        private int defaultInterval = 1000;
+        
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            liveSensorData = new SensorData();
+            ArduinoCom = new ArduinoComClass("COM4", 9600);
+            oThreadone = new Thread(endreVerdiArduino);
+            oThreadone.Start();
+            timer1.Interval = defaultInterval;
+            timer1.Start();
+            
+        }
+
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.txtBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                
+                this.txtBox.AppendText("PIR: " + liveSensorData.pir.ToString() + " \tTemp: " + liveSensorData.temp.ToString());
+                txtBox.AppendText("\r\n");
+                
+            }
+            
+        }
+
+        public void endreVerdiArduino()
+        {
+            string text = "";
+            while (true)
+            {
+                if (ArduinoCom.DataReady)
+                {
+                    text = ArduinoCom.receivedData;
+                    ArduinoCom.DataReady = false;
+                    SetText(text + "\n");
+                    string[] SensorData = text.Split(':');
+                    SensorData[1] = SensorData[1].Remove(SensorData[1].Length - 1);
+                    SensorData[1] = SensorData[1].Replace('.', ',');
+                    if (SensorData[0] == "1")
+                    {
+                        liveSensorData.pir = true;
+                    }
+                    else
+                    {
+                        liveSensorData.pir = false;
+                    }
+                    liveSensorData.temp = Convert.ToSingle(SensorData[1]);
+                }
+                Application.DoEvents();
+                Thread.Sleep(1000);
+            }
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ArduinoCom.Write("1");
+            
+        }
+
+        class ArduinoComClass : SerialPort
+        {
+            public string receivedData;
+            public bool DataReady;
+            public ArduinoComClass(string comport, int baudrate)
+            {
+
+                PortName = comport;
+                BaudRate = baudrate;
+                DataReceived += new SerialDataReceivedEventHandler(dataRecived);
+                ReadTimeout = 2000;
+                DataReady = false;
+                try
+                {
+                    Open();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+
+            public void dataRecived(object sender, SerialDataReceivedEventArgs earg)
+            {
+                try
+                {
+
+                    receivedData = ReadLine();
+                    DataReady = true;
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    DataReady = false;
+                }
+            }
+
+        }
+        struct SensorData
+        {
+            public float temp;
+            public bool pir;
+        }
+
+        private void cboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int interval;
+            interval = Convert.ToInt32(cboBox.Text);
+
+            switch (interval)
+            {
+
+                case 1:
+                    interval = 1000;
+                    break;
+                case 5:
+                    interval = 5000;
+                    break;
+                case 10:
+                    interval = 10000;
+                    break;
+                case 15:
+                    interval = 15000;
+                    break;
+                case 30:
+                    interval = 30000;
+                    break;
+            }
+            timer1.Interval = interval;
+            
+        }
+    }
+}
